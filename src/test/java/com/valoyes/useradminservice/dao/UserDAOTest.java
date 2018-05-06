b074003b413ca4473c56fac37dc2e60b011efe23
bavaloyes <bavaloyes@ITEM-S35533.emea.msad.sopra>
2018-05-06 13:11:37 +0200

    permite importar los metodos estaticos
    import static org.powermock.api.mockito.PowerMockito.*;

--- src/test/java/com/valoyes/useradminservice/dao/UserDAOTest.java
+++ src/test/java/com/valoyes/useradminservice/dao/UserDAOTest.java
@@ -1,23 +1,33 @@
 package com.valoyes.useradminservice.dao;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.valoyes.useradminservice.dto.User;
 import com.valoyes.useradminservice.util.IDGenerator;
-// permite importar los metodos estaticos
 import static org.powermock.api.mockito.PowerMockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(IDGenerator.class)
 public class UserDAOTest {
 
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void create_ShouldReturnUserIdBack() {
+
+		// given
+		UserDAO dao = new UserDAO();
+		
+		// powermockito
+		mockStatic(IDGenerator.class);
+		
+		dao.createDao(new User());
+		
+		
+		
 	}
 
 }
